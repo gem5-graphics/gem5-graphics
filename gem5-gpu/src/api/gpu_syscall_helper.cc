5c26bd12445c9fb8eb1949945e8d8245a7dca739
Ayub Gubran <agubran@google.com>
2017-07-27 11:18:09 -0700

    querying cache line size thorugh tc instead of ruby's getBlockSizeBytes()

--- gem5-gpu/src/api/gpu_syscall_helper.cc
+++ gem5-gpu/src/api/gpu_syscall_helper.cc
@@ -67,7 +67,8 @@ GPUSyscallHelper::readString(Addr addr, uint8_t* p, int size, ThreadContext *tc)
     bool null_not_found = true;
     Addr curr_addr;
     int read_size;
-    unsigned block_size = RubySystem::getBlockSizeBytes();
+    unsigned block_size = tc->getSystemPtr()->cacheLineSize();
+
     int bytes_read = 0;
     for (; bytes_read < size && null_not_found; bytes_read += read_size) {
         curr_addr = addr + bytes_read;
